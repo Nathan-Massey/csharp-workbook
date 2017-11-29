55dd8ccccfae2e252bf0de9e6334b99052cef99f
unknown <JCDenton46@gmail.com>
2017-11-28 18:33:11 -0600

    Made the needed fixes in the code to get the tic tac toe program to run.

--- checkpoint/checkpoint2.cs
+++ checkpoint/checkpoint2.cs
@@ -3,38 +3,22 @@ using System.Text;
 
 public class Program
 {
+    private static string playerTurn;
+
     public static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
         Game game = new Game();
 
         Console.ReadLine();
+
+   
     }
 } 
-public class Marker
-{
-    private static string playerTurn;
-
-    public static void GetInput()
-    {
-        Console.WriteLine("Player " + playerTurn);
-        Console.WriteLine("Enter Row:");
-        int row = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("Enter Column:");
-        int column = Int32.Parse(Console.ReadLine());
-
-        PlaceMark(row, column);
-    }
-
-    private static void PlaceMark(int row, int column)
-    {
-        throw new NotImplementedException();
-    }
-}
 
 public class Board
 {
-    public static string playerTurn = "X";
+    public string playerTurn = "X";
     public static string[][] board = new string[][]
     {
         new string[] {" ", " ", " "},
@@ -42,7 +26,97 @@ public class Board
         new string[] {" ", " ", " "}
     };
 
-    public static void DrawBoard()
+    public bool CheckForWin()
+    {
+        bool hasPlayerWon = HorizontalWin() || VerticalWin() || DiagonalWin();
+
+        if (hasPlayerWon)
+        {
+            Console.WriteLine("Player " + playerTurn + " won!");
+        }
+        else
+        {
+            playerTurn = playerTurn == "X" ? "O" : "X";
+        }
+
+        return hasPlayerWon;
+    }
+
+    bool HorizontalWin()
+    {
+        bool horizontalWin = false;
+
+        if (Board.board[0][0] == playerTurn && Board.board[0][1] == playerTurn && Board.board[0][2] == playerTurn)
+        {
+            horizontalWin = true;
+        }
+
+        if (Board.board[1][0] == playerTurn && Board.board[1][1] == playerTurn && Board.board[1][2] == playerTurn)
+        {
+            horizontalWin = true;
+        }
+
+        if (Board.board[2][0] == playerTurn && Board.board[2][1] == playerTurn && Board.board[2][2] == playerTurn)
+        {
+            horizontalWin = true;
+        }
+
+        return horizontalWin;
+    }
+
+    bool DiagonalWin()
+    {
+        bool diagonalWin = false;
+
+        if (Board.board[0][0] == playerTurn && Board.board[1][1] == playerTurn && Board.board[2][2] == playerTurn)
+        {
+            diagonalWin = true;
+        }
+
+        if (Board.board[2][0] == playerTurn && Board.board[1][1] == playerTurn && Board.board[0][2] == playerTurn)
+        {
+            diagonalWin = true;
+        }
+
+        return diagonalWin;
+    }
+
+    bool VerticalWin()
+    {
+        bool verticalWin = false;
+
+        if (Board.board[0][0] == playerTurn && Board.board[1][0] == playerTurn && Board.board[2][0] == playerTurn)
+        {
+            verticalWin = true;
+        }
+
+        if (Board.board[0][1] == playerTurn && Board.board[1][1] == playerTurn && Board.board[2][1] == playerTurn)
+        {
+            verticalWin = true;
+        }
+
+        if (Board.board[0][2] == playerTurn && Board.board[1][2] == playerTurn && Board.board[2][2] == playerTurn)
+        {
+            verticalWin = true;
+        }
+
+        return verticalWin;
+    }
+
+    bool CheckForTie()
+    {
+        bool havePlayersTied = false;
+
+        if (Board.board[0][0] != " " && Board.board[0][1] != " " && Board.board[0][2] != " " && Board.board[1][0] != " " && Board.board[1][1] != " "
+            && Board.board[1][2] != " " && Board.board[2][0] != " " && Board.board[2][1] != " " && Board.board[2][2] != " ")
+        {
+            havePlayersTied = true;
+        }
+
+        return havePlayersTied;
+    }
+
+    public void DrawBoard()
     {
         Console.WriteLine("  0 1 2");
         Console.WriteLine("0 " + String.Join("|", board[0]));
@@ -52,10 +126,12 @@ public class Board
         Console.WriteLine("2 " + String.Join("|", board[2]));
     }
 
-    public static void PlaceMark(int row, int column)
+    public void PlaceMark(int row, int column)
     {
         board[row][column] = playerTurn;
     }
+
+    
 }
 
 public class Game
@@ -64,101 +140,29 @@ public class Game
 
     public Game()
     {
-        Board board = new Board();
+        Board main = new Board();
         
 
-        bool CheckForWin()
+        do
         {
-            bool hasPlayerWon = HorizontalWin() || VerticalWin() || DiagonalWin();
+            main.DrawBoard();
+           
+            
+                Console.WriteLine("Player " + playerTurn);
+                Console.WriteLine("Enter Row:");
+                int row = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Column:");
+                int column = Int32.Parse(Console.ReadLine());
+                main.PlaceMark(row, column);
 
-            if (hasPlayerWon)
-            {
-                Console.WriteLine("You Won!!");
-            }
-            else
-            {
-                playerTurn = playerTurn == "X" ? "O" : "X";
-            }
+                Console.WriteLine("Player turn: " + playerTurn);
+            
 
-            return hasPlayerWon;
+
         }
+        while (!main.CheckForWin());
 
-        bool HorizontalWin()
-        {
-            bool horizontalWin = false;
-
-            if (Board.board[0][0] == playerTurn && Board.board[0][1] == playerTurn && Board.board[0][2] == playerTurn)
-            {
-                horizontalWin = true;
-            }
-
-            if (Board.board[1][0] == playerTurn && Board.board[1][1] == playerTurn && Board.board[1][2] == playerTurn)
-            {
-                horizontalWin = true;
-            }
-
-            if (Board.board[2][0] == playerTurn && Board.board[2][1] == playerTurn && Board.board[2][2] == playerTurn)
-            {
-                horizontalWin = true;
-            }
-
-            return horizontalWin;
-        }
-
-        bool DiagonalWin()
-        {
-            bool diagonalWin = false;
-
-            if (Board.board[0][0] == playerTurn && Board.board[1][1] == playerTurn && Board.board[2][2] == playerTurn)
-            {
-                diagonalWin = true;
-            }
-
-            if (Board.board[2][0] == playerTurn && Board.board[1][1] == playerTurn && Board.board[0][2] == playerTurn)
-            {
-                diagonalWin = true;
-            }
-
-            return diagonalWin;
-        }
-
-        bool VerticalWin()
-        {
-            bool verticalWin = false;
-
-            if (Board.board[0][0] == playerTurn && Board.board[1][0] == playerTurn && Board.board[2][0] == playerTurn)
-            {
-                verticalWin = true;
-            }
-
-            if (Board.board[0][1] == playerTurn && Board.board[1][1] == playerTurn && Board.board[2][1] == playerTurn)
-            {
-                verticalWin = true;
-            }
-
-            if (Board.board[0][2] == playerTurn && Board.board[1][2] == playerTurn && Board.board[2][2] == playerTurn)
-            {
-                verticalWin = true;
-            }
-
-            return verticalWin;
-        }
-
-        bool CheckForTie()
-        {
-            bool havePlayersTied = false;
-
-            if (Board.board[0][0] != " " && Board.board[0][1] != " " && Board.board[0][2] != " " && Board.board[1][0] != " " && Board.board[1][1] != " "
-                && Board.board[1][2] != " " && Board.board[2][0] != " " && Board.board[2][1] != " " && Board.board[2][2] != " ")
-            {
-                havePlayersTied = true;
-            }
-
-            return havePlayersTied;
-        }
+        
     }
-       
-
-
 }
 
